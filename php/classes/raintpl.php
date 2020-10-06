d5caf9f4ce14b0b4e8e89405e7ba0a197c535746
Patrick CHAUTARD <patrick.chautard@free.fr>
2020-10-06 10:18:26 +0200

    Modification du nom du répertoire pout les templates raintpl en html

--- php/classes/raintpl.php
+++ php/classes/raintpl.php
@@ -1,6 +1,9 @@
 <?php
 /**
- *
+ * raintpl.ph
+ * 
+ * Version : 1.0.0
+ * Date : 2020-10-06
  * @author CHAUTARD
  *
  */
@@ -9,7 +12,7 @@ include "raintpl/rain.tpl.class.php";
 
 raintpl::configure("base_url", null );
 raintpl::configure("path_replace", false);
-raintpl::configure("tpl_dir", str_replace('classes', 'templates', __DIR__ ) . DIRECTORY_SEPARATOR);
+raintpl::configure("tpl_dir", str_replace('classes', 'html', __DIR__ ) . DIRECTORY_SEPARATOR);
 raintpl::configure("cache_dir", str_replace('classes', 'cacheRainTpl', __DIR__ ) . DIRECTORY_SEPARATOR );
 
 //initialize a Rain TPL object
